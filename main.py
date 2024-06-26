5de1a825a6d07a1cc26f6b2fa28989819b9e20a6
lukintrees <lukintrees@gmail.com>
2024-06-26 20:34:06 +0300

    first implementation of main.py

--- main.py
+++ main.py
@@ -12,3 +12,56 @@
 # GNU General Public License for more details.
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import asyncio
+import logging
+from typing import List
+
+from openai import AsyncClient
+
+from config import get_config
+from platforms import Message, Provider
+from platforms.discord import DiscordProvider
+
+logging.basicConfig(level=logging.INFO)
+
+
+async def main():
+    config = get_config()
+    logging.debug(config)
+    client = AsyncClient(
+        api_key=config["api"]["key"], base_url=config["api"]["base_url"]
+    )
+    if config["bot"]["discord"]["self_bot"]:
+        provider: Provider = DiscordProvider()
+    else:
+        provider: Provider = DiscordProvider()
+    task = asyncio.create_task(provider.start())
+    async for message in provider.get_message_generator():
+        async with provider.typing(message):
+            messages = [{"role": "system", "content": config["bot"]["system_prompt"]}]
+            context = await provider.get_message_context(message)
+            context.append(message)
+            messages.extend(to_openai_messages(context))
+            print(messages)
+            response = await client.chat.completions.create(
+                messages=messages,
+                model=config["bot"]["model"],
+            )
+            await provider.send_message(response.choices[0].message.content, message)
+
+
+def to_openai_messages(messages: List[Message]) -> List:
+    result = []
+    for msg in messages:
+        result.append(
+            {
+                "role": "assistant" if msg.is_our else "user",
+                "content": msg.text if msg.is_our else f"{msg.author}:{msg.text}",
+            }
+        )
+    return result
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
